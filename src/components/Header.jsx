7a77ce97cc9d7688347bbedfe96469f84d78449f
A-O-Emmanuel <emmanuel0058@gmail.com>
2024-10-30 14:18:08 +0100

    chamged the color of the logo text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary nav">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">careerHunt</a>
+            <a className="navbar-brand nav__logo" href="#">careerHunt</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
