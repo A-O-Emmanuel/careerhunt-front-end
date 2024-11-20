02662689a70139430d38c01a27a3e8a8109894be
A-O-Emmanuel <emmanuel0058@gmail.com>
2024-11-20 19:47:42 +0100

    added boostrap spinner and added custom styles for it

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,13 @@
 function Loader() {
     return (
         <>
-            <p>fetching jobs...</p>
+            
+            <div className="d-flex justify-content-center">
+                <div className="spinner-border text-primary loader" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </div>
+           
         </>
     )
 }
