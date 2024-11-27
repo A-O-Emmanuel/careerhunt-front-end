9167e83c63d20f96b138c9ffa59ca4eb45c0e207
A-O-Emmanuel <emmanuel0058@gmail.com>
2024-11-27 02:51:10 +0100

    added the functionality to save jobs in the save job component in the frontend, also added the functionality to receive those saved jobs in the backend savejob route

--- src/components/SaveJobButton.jsx
+++ src/components/SaveJobButton.jsx
@@ -16,8 +16,37 @@ function handleSave() {
     contract, 
     applyLink, 
     setJobTitle});
-} 
+}
 
+
+useEffect(function() {
+    async function saveJobs() {
+
+        const jobInfo = {
+            jobTitle: saveJob.jobTitle,
+            company: saveJob.compay,
+            jobLocation: saveJob.jobLocation,
+            salayMax: saveJob.salaryMax,
+            salaryMin: saveJob.salaryMin,
+            description: saveJob.description,
+            contract: saveJob.contract,
+            applyLink: saveJob.applyLink,
+        }
+        
+        const response = await fetch('http://localhost:4000/savejob', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(jobInfo)
+    
+        });
+    
+        const res = await response.json();
+        console.log(res)
+    }
+    saveJobs()
+}, [saveJob])
     return (
         <>
             <button onClick={handleSave} className='single-job__container--save-job'><img src={favorite} alt="" />  Save Job</button>
