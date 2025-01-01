184b3e52e2ab209280c598e5e892b75061d07a03
A-O-Emmanuel <emmanuel0058@gmail.com>
2025-01-02 00:22:37 +0100

    added .env configuration to the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.VITE_APP_ID':JSON.stringify(process.env.VITE_APP_ID),
+    'process.env.VITE_APP_KEY':JSON.stringify(process.env.VITE_APP_KEY),
+  },
   test: {
     globals: true,
     environment: "jsdom",
