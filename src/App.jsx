bc00bfeeef63efc648fb37eef07927a27ab8638d
A-O-Emmanuel <emmanuel0058@gmail.com>
2025-01-01 11:03:38 +0100

    added lazy loading for the about, privacy policy and disclaimer pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import Navigation from './components/Navigation.jsx';
 import SignIn, {action as loginInfo} from './components/SignIn.jsx';
 import Register, {action as userData} from './components/Register.jsx';
 import {action as deleteUserAction} from './components/DeleteUser.jsx';
-import About from './components/About.jsx';
-import PrivacyPolicy from './components/PrivacyPolicy.jsx';
-import Disclaimers from './components/Disclaimers.jsx';
+//import About from './components/About.jsx';
+//import PrivacyPolicy from './components/PrivacyPolicy.jsx';
+//import Disclaimers from './components/Disclaimers.jsx';
 import {action as LogoutAction} from './components/Logout.jsx';
 import SavedJobs from './components/SavedJobs.jsx';
 import Layout from './components/Layout.jsx';
 import {tokenLoader} from './util/auth.js'
 import PasswordReset from './components/PasswordReset.jsx';
 import {action as PassworResetAction} from './components/PasswordReset.jsx';
+import { lazy, Suspense } from 'react';
+
+const About = lazy(() => import('./components/About.jsx'));
+const PrivacyPolicy = lazy(() => import('./components/PrivacyPolicy.jsx'));
+const Disclaimers = lazy(() => import('./components/Disclaimers.jsx'));
 
 
 
@@ -23,12 +28,12 @@ const router = createBrowserRouter([
     loader: tokenLoader,
     children: [
       {path: '/', element: <Navigation />},
-      {path: '/about', element: <About />},
+      {path: '/about', element: <Suspense fallback={<p>Loading</p>}><About /></Suspense>},
       {path: '/signin', element: <SignIn />, action: loginInfo},
       {path: '/register', element: <Register />, action: userData },
       {path: '/deleteUser', action: deleteUserAction },
-      {path: '/privacypolicies', element: <PrivacyPolicy />},
-      {path: '/disclaimers', element: <Disclaimers />},
+      {path: '/privacypolicies', element: <Suspense fallback={<p>Loading</p>}><PrivacyPolicy /></Suspense>},
+      {path: '/disclaimers', element: <Suspense fallback={<p>Loading</p>}><Disclaimers /></Suspense>},
       {path: '/logout', action: LogoutAction},
       {path: '/savedjobs', element: <SavedJobs />},
       {path: '/forgotpassword', element: <PasswordReset />, action: PassworResetAction}
