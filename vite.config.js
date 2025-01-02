0213fc6eb43316008a791cf539a04e6bbcd53e55
A-O-Emmanuel <emmanuel0058@gmail.com>
2025-01-02 13:26:11 +0100

    restored vite.config.js to default

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    VITE_APP_ID:process.env.VITE_APP_ID,
-    VITE_APP_KEY:process.env.VITE_APP_KEY
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
 })
